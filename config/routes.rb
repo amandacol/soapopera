d377b3d21677f2830653084ea1470308f880d955
Estelle Quezel <estellequezel@gmail.com>
2020-02-27 12:01:37 -0300

    Resolving conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-resources :soaps, only: [:index, :show, :new, :create]
-=======
-  resources :soaps, only: [:index, :new, :create, :show]
->>>>>>> 07a78058b277b3e23da014ba55d0caf4d5856ac7
+  resources :soaps, only: [:index, :show, :new, :create]
+
 end
