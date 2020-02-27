e01dc63f6477378156ac1a980262ee32b38d7d53
Amanda Bezerra <colmenero88@bol.com.br>
2020-02-27 16:37:42 -0300

    defining order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,24 @@
 class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+  def create
+    @soap = Soap.find(params[:soap_id])
+    @order = Order.new
+    @order.soap = @soap
+    @order.user = current_user
+    @order.date = Time.now
+    @order.save
+    redirect_to root_path
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:date)
+  end
 end
