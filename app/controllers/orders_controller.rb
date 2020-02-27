7d0a82a20d5cbe67731f952e627ad5817cc7016d
Amanda Bezerra <colmenero88@bol.com.br>
2020-02-27 18:05:01 -0300

    pundit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,17 @@
 class OrdersController < ApplicationController
   def new
     @order = Order.new
+    authorize @order
   end
   def create
     @soap = Soap.find(params[:soap_id])
     @order = Order.new
+    authorize @order
     @order.soap = @soap
     @order.user = current_user
     @order.date = Time.now
     @order.save
-    redirect_to root_path
+    redirect_to soaps_path, notice: "The soap was added to your cart!"
   end
 
   private
--- app/controllers/soaps_controller.rb
+++ app/controllers/soaps_controller.rb
@@ -3,26 +3,37 @@ class SoapsController < ApplicationController
 
   def index
     @soaps = Soap.all
+    @soaps = policy_scope(Soap).order(created_at: :desc)
   end
 
   def show
     @soap = Soap.find(params[:id])
+    authorize @soap
     @order = Order.new
     @user = @soap.user
   end
 
   def new
     @soap = Soap.new
+    authorize @soap
   end
 
   def create
     @soap = Soap.new(soap_params)
+    authorize @soap
     @soap.user = current_user
     if @soap.save!
       redirect_to soap_path(@soap)
     else
       render :new
     end
+  end
+
+  def destroy
+    @soap = Soap.find(params[:id])
+    authorize @soap
+    @soap.destroy
+    redirect_to soap_path
   end
 
   private
